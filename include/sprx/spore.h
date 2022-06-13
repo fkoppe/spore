8be0fbcb5d46a704921845612f833bb64371cede
Felix Koppe <fkoppe@web.de>
2022-06-13 23:01:40 +0200

    Create rough project structure

---
+++ include/sprx/spore.h
@@ -0,0 +1,26 @@
+/************************************************************************************
+*                                                                                   *
+*   spore - https://github.com/fkoppe/spore                                         *
+*   ************************************************************************        *
+*                                                                                   *
+*   Copyright (C) 2022 Felix Koppe <fkoppe@web.de>                                  *
+*                                                                                   *
+*   This program is free software: you can redistribute it and/or modify            *
+*   it under the terms of the GNU Affero General Public License as published        *
+*   by the Free Software Foundation, either version 3 of the License, or            *
+*   (at your option) any later version.                                             *
+*                                                                                   *
+*   This program is distributed in the hope that it will be useful,                 *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of                  *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                   *
+*   GNU Affero General Public License for more details.                             *
+*                                                                                   *
+*   You should have received a copy of the GNU Affero General Public License        *
+*   along with this program.  If not, see <https://www.gnu.org/licenses/>.          *
+*                                                                                   *
+************************************************************************************/
+
+#ifndef ___SPRX___SPORE_H
+#define ___SPRX___SPORE_H
+
+#endif // ___SPRX___SPORE_H
